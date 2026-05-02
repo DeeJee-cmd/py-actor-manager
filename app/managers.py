12dd1b32dd4ba1124e5e3855bea6782c15f7eb8e
Pasha Liashenko <zedxi236@ukr.net>
2026-05-02 19:27:49 +0300

    Solution

--- app/managers.py
+++ app/managers.py
@@ -19,9 +19,14 @@ class ActorManager:
 
     def all(self) -> list[Actor]:
         cursor = self._connection.execute(f"SELECT * FROM {self.table_name}")
-        row = cursor.fetchall()
-        return [Actor(id=row[0], first_name=row[1], last_name=row[2])
-                for row in row]
+        return [
+            Actor(id=row[0],
+                  first_name=row[1],
+                  last_name=row[2]
+                  ) for row in self._connection.execute(
+                f'SELECT * FROM {self.table_name}'
+            )
+                ]
 
     def update(self, pk: int, new_first_name: str, new_last_name: str) -> None:
         self._connection.execute(
