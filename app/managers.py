016bdcd643b8abd922d4bea45db9b900997b1866
Pasha Liashenko <zedxi236@ukr.net>
2026-05-02 19:15:06 +0300

    Solution

--- app/managers.py
+++ app/managers.py
@@ -4,14 +4,16 @@ from app.models import Actor
 
 
 class ActorManager:
-    def __init__(self, db_name, table_name):
+    def __init__(self, db_name: str, table_name: str) -> None:
         self.db_name = db_name
         self.table_name = table_name
         self._connection = sqlite3.connect(db_name)
 
     def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)",
+            f"""
+                INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)
+                """,
             (first_name, last_name)
         )
         self._connection.commit()
